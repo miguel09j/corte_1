28ef661805b1aecde7e86976093a0dadbd2db7d4
miguel09j <110635601+miguel09j@users.noreply.github.com>
2023-08-28 17:53:25 -0500

    Delete clase_2 directory

--- clase_2/aprobo.py
+++
@@ -1,8 +0,0 @@
-n = float(input("ingrese su nota: "))
-
-if n >=3:
-    print("aprobo")
-elif n <= 3:
-    print("reprobo")
-else:
-    print("ingrse una nota valida")
--- clase_2/area.py
+++
@@ -1,7 +0,0 @@
-r = float(input("ingrese un numero: "))
-
-if r != 0:
-    A = 3.14*(r**2)
-    print("el resultado es: ", A )
-else:
-    print("ingrse un numero valido")
