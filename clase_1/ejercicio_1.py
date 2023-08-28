49b61e1bccb6dd28cc681844b329bb85b4a3febd
miguel09j <110635601+miguel09j@users.noreply.github.com>
2023-08-28 17:53:11 -0500

    Delete clase_1 directory

--- clase_1/ejercicio_1.py
+++
@@ -1,8 +0,0 @@
-n = int(input("ingrese un numero: "))
-r = n%2
-if (r==0):
-    print("el numero es par")
-else:
-    print("el numero es impar")
-
-
--- clase_1/ejercicio_2.py
+++
@@ -1,4 +0,0 @@
-r =int(input("ingrese el radio en metros: "))
-v = (3/4)*(3.14*(r^3))
-r = v * 1000
-print("el resultado es = ", r)
